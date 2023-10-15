8d9dfff1a1cc89983c91c1b22492c5e510ee3587
cr08 <Vchat20@gmail.com>
2023-10-15 17:19:11 -0400

    Repo cleanup
    As progress has been made on this fork, I'm removing old, unneeded code.

--- process.py
+++ process.py
@@ -8,6 +8,7 @@ try:
     input = open("addresses.db")
 except OSError as error:
     print("SQLite database addresses.db not found. Please run the import.py script to generate an address database. Review the README.md for more information.")
+    sys.exit(1)
 
 connection = sqlite3.connect('addresses.db')
 cursor = connection.cursor()
